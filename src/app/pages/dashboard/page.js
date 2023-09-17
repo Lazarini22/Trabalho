bf21946c3020ca8a257578e783b66901ac6b8766
Renato Borges Lazarini <renatoborgeslazarini14@gmail.com>
2023-09-17 16:36:11 -0300

    Listagem

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,12 +1,17 @@
+import Listagem from "@/app/Listagem/Listagem";
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { Suspense } from "react";
 
 export default async function Dashboard() {
     let nomes = getUsers();
     return (
         <div>
-            {nomes.map(serv => (
-                <h1>{serv.nome}</h1>
-            ))}
+            <Suspense fallback={<p>Carregando</p>}>
+                <Listagem users={nomes}/>
+                {nomes.map(serv => (
+                    <h1>{serv.nome}</h1>
+                ))}
+            </Suspense>
         </div>
     );
 };
